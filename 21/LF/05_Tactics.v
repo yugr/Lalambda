430c69819d030c86db61b58db74a9223d2efc0b5
Yury Gribov <tetra2005@gmail.com>
2021-06-26 10:25:00 +0300

    Finish LF Tactics chapter.

--- 21/LF/05_Tactics.v
+++ 21/LF/05_Tactics.v
@@ -222,3 +222,152 @@ destruct b.
   + rewrite Hfalse.
     assumption.
 Qed.
+
+Notation "x =? y" := (eqb x y).
+
+Theorem eqb_sym :
+  forall (n m : nat),
+  (n =? m) = (m =? n).
+Proof.
+intros n.
+induction n.
+- simpl.
+  destruct m.
+  + simpl.
+    reflexivity.
+  + simpl.
+    reflexivity.
+- destruct m.
+  + simpl.
+    reflexivity.
+  + simpl.
+    apply IHn.
+Qed.
+
+Theorem eqb_trans :
+  forall (n m p : nat),
+  n =? m = true ->
+  m =? p = true ->
+  n =? p = true.
+Proof.
+intros n.
+induction n.
+- intros.
+  destruct m.
+  + assumption.
+  + simpl in H.
+    discriminate H.
+- intros.
+  destruct m.
+  + simpl in H.
+    discriminate H.
+  + simpl in H.
+    destruct p.
+    * simpl in H0.
+      discriminate H0.
+    * simpl.
+      apply IHn with (m := m).
+      ** assumption.
+      ** simpl in H0.
+         assumption.
+Qed.
+
+Lemma len_0_is_nil :
+  forall X (l : list X),
+  length l = 0 -> l = nil.
+Proof.
+intros.
+destruct l.
+- reflexivity.
+- simpl in H.
+  discriminate H.
+Qed.
+
+Theorem split_combine :
+  forall X Y (l : list (X * Y)) l1 l2,
+  length l1 = length l2 -> combine l1 l2 = l -> split l = (l1, l2).
+Proof.
+intros X Y l.
+induction l.
+- intros.
+  destruct l1.
+  + simpl.
+    simpl in H.
+    symmetry in H.
+    apply len_0_is_nil in H.
+    rewrite H.
+    reflexivity.
+  + intros.
+    simpl.
+Admitted.
+
+Fixpoint filter {X : Type} (test : X -> bool) (l : list X) :=
+  match l with
+  | nil    => nil
+  | x :: l' => if test x then x :: filter test l' else filter test l'
+  end.
+
+Theorem filter_true :
+  forall X (test : X -> bool) (x : X) (l lf : list X),
+  filter test l = x :: lf -> test x = true.
+Proof.
+intros X test x.
+induction l.
+- intros.
+  simpl in H.
+  discriminate H.
+- intros.
+  simpl in H.
+  destruct (test a) eqn:Ha.
+  + injection H.
+    intros.
+    rewrite <- H1.
+    assumption.
+  + apply IHl in H.
+    assumption.
+Qed.
+
+Fixpoint forallb {X} (test : X -> bool) (l : list X) : bool :=
+  match l with
+  | nil     => true
+  | x :: l' => if test x then forallb test l' else false
+  end.
+
+Fixpoint existsb {X} (test : X -> bool) (l : list X) : bool :=
+  match l with
+  | nil     => false
+  | x :: l' => if test x then true else existsb test l'
+  end.
+
+Definition existsb' {X} (test : X -> bool) (l : list X) :=
+  negb (forallb (fun (x : X) => negb (test x)) l).
+
+Theorem existsb_existsb' :
+  forall X (test : X -> bool) (l : list X),
+  existsb test l = existsb' test l.
+Proof.
+intros X test.
+induction l.
+- simpl.
+  unfold existsb'.
+  unfold forallb.
+  simpl.
+  reflexivity.
+- destruct (test a) eqn:Ha.
+  + simpl.
+    rewrite Ha.
+    unfold existsb'.
+    unfold forallb.
+    rewrite Ha.
+    simpl.
+    reflexivity.
+  + simpl.
+    rewrite Ha.
+    unfold existsb'.
+    unfold forallb.
+    rewrite Ha.
+    simpl.
+    fold (forallb (fun x : X => negb (test x)) l).
+    fold (existsb' test l).
+    assumption.
+Qed.
